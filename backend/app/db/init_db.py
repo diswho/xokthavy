6551fe7d21369663c45c71be85a7748afa20da16
diswho <discover.who@gmail.com>
2023-11-29 21:30:56 +0700

    29-11-2023

--- backend/app/db/init_db.py
+++ backend/app/db/init_db.py
@@ -21,15 +21,15 @@ def init_db(db: Session) -> None:
             full_name=settings.FIRST_SUPERUSER,
             is_superuser=True
         )
-        role_user_in = RoleCreate(name="user")
-        role_moderator_in = RoleCreate(name="moderator")
-        role_admin_in = RoleCreate(name="admin")
+        # role_user_in = RoleCreate(name="user")
+        # role_moderator_in = RoleCreate(name="moderator")
+        # role_admin_in = RoleCreate(name="admin")
         try:
             user = crud_user.create(db=db, user=user_in)
-            role_user = crud_role.create_role(db=db, role=role_user_in)
-            role_moderator = crud_role.create_role(
-                db=db, role=role_moderator_in)
-            role_admin = crud_role.create_role(db=db, role=role_admin_in)
+            # role_user = crud_role.create_role(db=db, role=role_user_in)
+            # role_moderator = crud_role.create_role(
+            #     db=db, role=role_moderator_in)
+            # role_admin = crud_role.create_role(db=db, role=role_admin_in)
             # user_in.roles = [role_user_in, role_moderator_in, role_admin_in]
         except:
             print("====== Error")
--- backend/app/initial_data_schema.py
+++ backend/app/initial_data_schema.py
@@ -4,7 +4,10 @@ from app.db.session import SessionLocal
 from app.model import User, Role, RoleUser
 from app.db.base import Base
 from app.db.session import engine
-# from app.db.init_db import init_db
+from app.schemas.role import RoleCreate
+from app.schemas.user import UserCreate
+from app.crud import crud_role
+from app.core.config import settings
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -14,30 +17,42 @@ def init() -> None:
     Base.metadata.create_all(bind=engine)
     session = SessionLocal()
 
-    role_user = Role(name="user")
-    role_moderator = Role(name="moderator")
-    role_admin = Role(name="admin")
-    session.add_all([role_user, role_moderator, role_admin])
-    session.commit()
+    role_user = RoleCreate(name="user")
+    role_moderator = RoleCreate(name="moderator")
+    role_admin = RoleCreate(name="admin")
 
-    user_normal = User(full_name="normal", email="superadmin1", hashed_password="")
-    user_superadmin = User(full_name="superadmin",
-                 email="superadmin", hashed_password="")
+    crud_role_user = crud_role.create_role(db=session, role=role_user)
+    crud_role_moderator = RoleCreate(db=session, role=role_moderator)
+    crud_role_admin = RoleCreate(db=session, naroleme=role_admin)
 
-    session.add_all([user_normal, user_superadmin])
-    session.commit()
+    user_in = UserCreate(
+        email=settings.FIRST_SUPERUSER,
+        password=settings.FIRST_SUPERUSER_PASSWORD,
+        full_name=settings.FIRST_SUPERUSER,
+        is_superuser=True
+    )
 
-    role_user1 = RoleUser(role_id=role_user.id, user_id=user_normal.id,
-                          remark="Blue wrote chapter 1")
-    role_user2 = RoleUser(role_id=role_moderator.id, user_id=user_normal.id,
-                          remark="Chip wrote chapter 2")
-    role_user3 = RoleUser(role_id=role_moderator.id, user_id=user_superadmin.id,
-                          remark="Blue wrote chapters 1-3")
-    role_user4 = RoleUser(role_id=role_admin.id, user_id=user_superadmin.id,
-                          remark="Alyssa wrote chapter 4")
+    # session.add_all([role_user, role_moderator, role_admin])
+    # session.commit()
 
-    session.add_all([role_user1, role_user2, role_user3, role_user4])
-    session.commit()
+    # user_normal = User(full_name="normal", email="superadmin1", hashed_password="")
+    # user_superadmin = User(full_name="superadmin",
+    #              email="superadmin", hashed_password="")
+
+    # session.add_all([user_normal, user_superadmin])
+    # session.commit()
+
+    # role_user1 = RoleUser(role_id=role_user.id, user_id=user_normal.id,
+    #                       remark="Blue wrote chapter 1")
+    # role_user2 = RoleUser(role_id=role_moderator.id, user_id=user_normal.id,
+    #                       remark="Chip wrote chapter 2")
+    # role_user3 = RoleUser(role_id=role_moderator.id, user_id=user_superadmin.id,
+    #                       remark="Blue wrote chapters 1-3")
+    # role_user4 = RoleUser(role_id=role_admin.id, user_id=user_superadmin.id,
+    #                       remark="Alyssa wrote chapter 4")
+
+    # session.add_all([role_user1, role_user2, role_user3, role_user4])
+    # session.commit()
 
 
 def main() -> None:
